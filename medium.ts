76e096af2a0d3580a1ec3aa4e23e2f79be480da1
Jasmin Durakovic <jasmindurakovic13@gmail.com>
2022-04-06 23:23:21 +0200

    8. Readonly 2

--- medium.ts
+++ medium.ts
@@ -7,3 +7,8 @@ type MyReturnType<T> = T extends (...args: any[]) => infer T ? T : never
 // https://github.com/type-challenges/type-challenges/blob/master/questions/3-medium-omit/README.md
 
 type MyOmit<T, K> = { [key in Exclude<keyof T, K>]: T[key] }
+
+// 8. Readonly 2
+// https://github.com/jasmin92/type-challenges/blob/master/questions/8-medium-readonly-2/README.md
+
+type MyReadonly2<T, K extends keyof T = keyof T> = Readonly<Pick<T, K>> & { [key in Exclude<keyof T, K>]: T[key] }
