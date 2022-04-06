f5487db1071400161d9d2219c1235cc6fa140ca4
Jasmin Durakovic <jasmindurakovic13@gmail.com>
2022-04-06 21:20:02 +0200

    3. Omit

--- medium.ts
+++ medium.ts
@@ -2,3 +2,8 @@
 // https://github.com/jasmin92/type-challenges/blob/master/questions/2-medium-return-type/README.md
 
 type MyReturnType<T> = T extends (...args: any[]) => infer T ? T : never
+
+// 3. Omit
+// https://github.com/type-challenges/type-challenges/blob/master/questions/3-medium-omit/README.md
+
+type MyOmit<T, K> = { [key in Exclude<keyof T, K>]: T[key] }
